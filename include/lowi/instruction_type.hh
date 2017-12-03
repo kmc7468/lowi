450d71d36dff797af2fddf3acbdd0b0efe2066e1
kmc7468 <kmc7468@naver.com>
2017-12-03 16:32:58 +0900

    instruction_type_set 클래스 추가

--- include/lowi/instruction_type.hh
+++ include/lowi/instruction_type.hh
@@ -1,10 +1,15 @@
 #ifndef LOWI_HEADER_INSTRUCTION_TYPE_HH
 #define LOWI_HEADER_INSTRUCTION_TYPE_HH
 
+#include <cstdint>
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace lowi
 {
+	class instruction_type_set;
+
 	class instruction_type
 	{
 	public:
@@ -27,6 +32,44 @@ namespace lowi
 	public:
 		virtual bool equal(const instruction_type& instruction_type) const = 0;
 		bool equal(const ptr& instruction_type) const;
+	
+	private:
+		std::string name_;
+		std::uint64_t number_of_operands_;
+	};
+
+	class instruction_type_set final
+	{
+	public:
+		using ptr = std::shared_ptr<instruction_type_set>;
+
+	public:
+		instruction_type_set() = default;
+		instruction_type_set(const instruction_type_set& instruction_type);
+		instruction_type_set(instruction_type_set&& instruction_type) noexcept;
+		~instruction_type_set() = default;
+
+	public:
+		instruction_type_set& operator=(const instruction_type_set& instruction_type);
+		instruction_type_set& operator=(instruction_type_set&& instruction_type) noexcept;
+		bool operator==(const instruction_type_set& instruction_type) const;
+		bool operator==(const ptr& instruction_type) const;
+		bool operator!=(const instruction_type_set& instruction_type) const;
+		bool operator!=(const ptr& instruction_type) const;
+
+	public:
+		instruction_type_set& assign(const instruction_type_set& instruction_type);
+		instruction_type_set& assign(instruction_type_set&& instruction_type) noexcept;
+		bool equal(const instruction_type_set& instruction_type) const;
+		bool equal(const ptr& instruction_type) const;
+
+	public:
+		std::string name() const;
+		std::string name(const std::string& new_name);
+
+	private:
+		std::string name_;
+		std::vector<instruction_type::ptr> instruction_types_;
 	};
 }
 
--- src/lowi/instruction_type.cc
+++ src/lowi/instruction_type.cc
@@ -1,5 +1,7 @@
 #include <lowi/instruction_type.hh>
 
+#include <utility>
+
 namespace lowi
 {
 	bool instruction_type::operator==(const instruction_type& instruction_type) const
@@ -22,5 +24,85 @@ namespace lowi
 	bool instruction_type::equal(const ptr& instruction_type) const
 	{
 		return equal(*instruction_type);
+	}
+}
+
+namespace lowi
+{
+	instruction_type_set::instruction_type_set(const instruction_type_set& instruction_type)
+		: name_(instruction_type.name_), instruction_types_(instruction_type.instruction_types_)
+	{}
+	instruction_type_set::instruction_type_set(instruction_type_set&& instruction_type) noexcept
+		: name_(std::move(instruction_type.name_)), instruction_types_(std::move(instruction_type.instruction_types_))
+	{}
+
+	instruction_type_set& instruction_type_set::operator=(const instruction_type_set& instruction_type)
+	{
+		return assign(instruction_type);
+	}
+	instruction_type_set& instruction_type_set::operator=(instruction_type_set&& instruction_type) noexcept
+	{
+		return assign(std::move(instruction_type));
+	}
+	bool instruction_type_set::operator==(const instruction_type_set& instruction_type) const
+	{
+		return equal(instruction_type);
+	}
+	bool instruction_type_set::operator==(const ptr& instruction_type) const
+	{
+		return equal(instruction_type);
+	}
+	bool instruction_type_set::operator!=(const instruction_type_set& instruction_type) const
+	{
+		return !equal(instruction_type);
+	}
+	bool instruction_type_set::operator!=(const ptr& instruction_type) const
+	{
+		return !equal(instruction_type);
+	}
+
+	instruction_type_set& instruction_type_set::assign(const instruction_type_set& instruction_type)
+	{
+		name_ = instruction_type.name_;
+		instruction_types_ = instruction_type.instruction_types_;
+		return *this;
+	}
+	instruction_type_set& instruction_type_set::assign(instruction_type_set&& instruction_type) noexcept
+	{
+		name_ = std::move(instruction_type.name_);
+		instruction_types_ = std::move(instruction_type.instruction_types_);
+		return *this;
+	}
+	bool instruction_type_set::equal(const instruction_type_set& instruction_type) const
+	{
+		if (this == &instruction_type)
+			return true;
+		if (name_ != instruction_type.name_)
+			return false;
+		if (instruction_types_.size() != instruction_type.instruction_types_.size())
+			return false;
+
+		for (std::size_t i = 0; i < instruction_types_.size(); ++i)
+		{
+			if (!instruction_types_[i]->equal(instruction_type.instruction_types_[i]))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+	bool instruction_type_set::equal(const ptr& instruction_type) const
+	{
+		return equal(*instruction_type);
+	}
+
+	std::string instruction_type_set::name() const
+	{
+		return name_;
+	}
+	std::string instruction_type_set::name(const std::string& new_name)
+	{
+		return name_ = new_name;
 	}
 }
