e0163d1cdfbfb8cc9b60035a90433fddafba98b6
kmc7468 <kmc7468@naver.com>
2017-12-04 19:34:00 +0900

    register_map_item_basic 클래스 추가

--- include/lowi/register_map.hh
+++ include/lowi/register_map.hh
@@ -6,16 +6,25 @@
 
 namespace lowi
 {
+	class register_map_item_basic;
+	class register_map_item_other;
+	class register_map_item_register;
+
 	class register_map_item
 	{
 	public:
 		using ptr = std::shared_ptr<register_map_item>;
+		using basic = register_map_item_basic;
+		using other = register_map_item_other;
+		using register_ = register_map_item_register;
 
 	public:
-		register_map_item() = delete;
 		register_map_item(const register_map_item& map_item) = delete;
 		register_map_item(register_map_item&& map_item) noexcept = delete;
 		virtual ~register_map_item() = default;
+
+	protected:
+		register_map_item() = default;
 
 	public:
 		register_map_item& operator=(const register_map_item& map_item) = delete;
@@ -31,6 +40,34 @@ namespace lowi
 
 	public:
 		virtual std::size_t size() const = 0;
+	};
+
+	class register_map_item_basic final : public register_map_item
+	{
+	public:
+		register_map_item_basic() noexcept;
+		register_map_item_basic(std::size_t size) noexcept;
+		register_map_item_basic(const register_map_item_basic& map_item) noexcept;
+		register_map_item_basic(register_map_item_basic&& map_item) noexcept;
+		virtual ~register_map_item_basic() override = default;
+	
+	public:
+		register_map_item_basic& operator=(const register_map_item_basic& map_item) noexcept;
+		register_map_item_basic& operator=(register_map_item_basic&& map_item) noexcept;
+		bool operator==(const register_map_item_basic& map_item) const noexcept;
+		bool operator!=(const register_map_item_basic& map_item) const noexcept;
+
+	public:
+		register_map_item_basic& assign(const register_map_item_basic& map_item) noexcept;
+		register_map_item_basic& assign(register_map_item_basic&& map_item) noexcept;
+		bool equal(const register_map_item_basic& map_item) const noexcept;
+
+	public:
+		std::size_t size() const noexcept;
+		std::size_t size(std::size_t new_size) noexcept;
+
+	private:
+		std::size_t size_;
 	};
 
 	class register_map final
--- src/lowi/register_map.cc
+++ src/lowi/register_map.cc
@@ -29,6 +29,63 @@ namespace lowi
 
 namespace lowi
 {
+	register_map_item_basic::register_map_item_basic() noexcept
+		: size_(0)
+	{}
+	register_map_item_basic::register_map_item_basic(std::size_t size) noexcept
+		: size_(size)
+	{}
+	register_map_item_basic::register_map_item_basic(const register_map_item_basic& map_item) noexcept
+		: size_(map_item.size_)
+	{}
+	register_map_item_basic::register_map_item_basic(register_map_item_basic&& map_item) noexcept
+		: size_(std::move(map_item.size_))
+	{}
+
+	register_map_item_basic& register_map_item_basic::operator=(const register_map_item_basic& map_item) noexcept
+	{
+		return assign(map_item);
+	}
+	register_map_item_basic& register_map_item_basic::operator=(register_map_item_basic&& map_item) noexcept
+	{
+		return assign(std::move(map_item));
+	}
+	bool register_map_item_basic::operator==(const register_map_item_basic& map_item) const noexcept
+	{
+		return equal(map_item);
+	}
+	bool register_map_item_basic::operator!=(const register_map_item_basic& map_item) const noexcept
+	{
+		return !equal(map_item);
+	}
+
+	register_map_item_basic& register_map_item_basic::assign(const register_map_item_basic& map_item) noexcept
+	{
+		size_ = map_item.size_;
+		return *this;
+	}
+	register_map_item_basic& register_map_item_basic::assign(register_map_item_basic&& map_item) noexcept
+	{
+		size_ = std::move(map_item.size_);
+		return *this;
+	}
+	bool register_map_item_basic::equal(const register_map_item_basic& map_item) const noexcept
+	{
+		return size_ == map_item.size_;
+	}
+
+	std::size_t register_map_item_basic::size() const noexcept
+	{
+		return size_;
+	}
+	std::size_t register_map_item_basic::size(std::size_t new_size) noexcept
+	{
+		return size_ = new_size;
+	}
+}
+
+namespace lowi
+{
 	register_map::register_map(const std::vector<register_map_item::ptr>& map)
 		: map_(map)
 	{}
